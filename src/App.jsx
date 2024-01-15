afef70136d3dbb865ef330feca700f4987b88aa7
Laíza  Pimentel <laizahpimentel@gmail.com>
2024-01-14 21:39:57 -0300

    First commit 140124

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+
+
+function App() {
+
+  const [todos,setTodos] = useState([
+    {
+      id:1,
+      text: "criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar constantemente",
+      category: "Estudo",
+      isCompleted: false,
+    },
+  ]);
+
+  return <div className="App">
+    <h1>Lista de tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p>{todo.category}</p>({todo.category})
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>;
+}
+
+export default App;
